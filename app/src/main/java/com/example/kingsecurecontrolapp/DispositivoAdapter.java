212f6738ec570485920157a7f31e3687e30ef014
JesusTur <al375859@uji.es>
2021-05-29 13:15:21 +0200

    Lista disp no asignados

--- app/src/main/java/com/example/kingsecurecontrolapp/DispositivoAdapter.java
+++ app/src/main/java/com/example/kingsecurecontrolapp/DispositivoAdapter.java
@@ -1,4 +1,100 @@
 package com.example.kingsecurecontrolapp;
 
-public class DispositivoAdapter {
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.example.kingsecurecontrolapp.Controlador.CasaController;
+import com.example.kingsecurecontrolapp.exceptions.HabitacionConDispositivosException;
+import com.example.kingsecurecontrolapp.exceptions.HabitacionNoExistenteException;
+import com.example.kingsecurecontrolapp.exceptions.HabitacionYaExistenteException;
+import com.example.kingsecurecontrolapp.modelo.Casa;
+import com.example.kingsecurecontrolapp.modelo.Dispositivo;
+import com.example.kingsecurecontrolapp.modelo.Habitacion;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
+public class DispositivoAdapter extends RecyclerView.Adapter<DispositivoAdapter.ViewHolder>{
+    private ArrayList<Dispositivo> localDataSet;
+    private Casa casa;
+    private Context context;
+    private RequestQueue requestQueue;
+    private CasaController casaController;
+    private Habitacion habitacion;
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.dspTlt);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     *
+     * by RecyclerView.
+     */
+    public DispositivoAdapter(Casa casa, Context context, RequestQueue requestQueue, CasaController casaController, Habitacion habitacion) {
+        this.casa = casa;
+        this.habitacion = habitacion;
+        this.context = context;
+        this.requestQueue=requestQueue;
+        this.casaController = casaController;
+        localDataSet = new ArrayList<>();
+        localDataSet.addAll(habitacion.getSensores());
+        localDataSet.addAll(habitacion.getActuadores());
+    }
+    public void dataSetChanged() throws InterruptedException {
+        notifyDataSetChanged();
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public DispositivoAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_dispositivo, viewGroup, false);
+
+        return new DispositivoAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DispositivoAdapter.ViewHolder holder, int position) {
+        holder.textView.setText(localDataSet.get(position).getNombre());
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.size();
+    }
 }
--- app/src/main/java/com/example/kingsecurecontrolapp/DispositivosNoAsignados.java
+++ app/src/main/java/com/example/kingsecurecontrolapp/DispositivosNoAsignados.java
@@ -3,19 +3,35 @@ package com.example.kingsecurecontrolapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.example.kingsecurecontrolapp.Controlador.CasaController;
+import com.example.kingsecurecontrolapp.Controlador.ControllerFactory;
 import com.example.kingsecurecontrolapp.modelo.Casa;
 
 public class DispositivosNoAsignados extends AppCompatActivity {
     private Casa casa;
     DispositivoAdapter dispositivoAdapter;
+    ControllerFactory controllerFactory;
+    CasaController casaController;
+    RequestQueue requestQueue;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dispositivos_fragment);
         getSupportActionBar().setTitle("Dispositivos disponibles");
         casa = (Casa) getIntent().getSerializableExtra("casa");
-        System.out.println(casa.getSinAsignar().getActuadores().size());
+        controllerFactory = new ControllerFactory();
+        casaController = controllerFactory.newCasaController();
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        dispositivoAdapter = new DispositivoAdapter(casa, this, requestQueue, casaController, casa.getSinAsignar());
+        recyclerView = findViewById(R.id.recylcerViewDispNoAsig);
+        recyclerView.setAdapter(dispositivoAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
